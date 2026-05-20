f85a081e6d0c1eb4e5675d251d2650624217344f
ljs528022 <ljs528022@gmail.com>
2026-05-21 08:52:59 +0900

    init

--- src/main/java/com/app/globalgates/controller/ai/AiAPIController.java
+++ src/main/java/com/app/globalgates/controller/ai/AiAPIController.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @Slf4j
 @RequestMapping("/ai/**")
 public class AiAPIController {
-    private final WebClient webClient = WebClient.create("http://localhost:8000");
+    private final WebClient webClient = WebClient.create("https://persons-pos-describes-tions.trycloudflare.com");
 
     // 게시글 신뢰도 측정
     @PostMapping("post/trust")
